836008d78b21c9d9b0f0cbd213ad000c2615935c
mohanji <mohan.bmdl@gmail.com>
2024-12-06 19:00:38 -0800

    'updatedconfig'

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
     env: {
         API_SECRET:'fe1f54bfe790f712bd4e352c79c4dea09f543932fd8f31279c3b84b5a221125f',
         TOKEN : process.env.TOKEN,
